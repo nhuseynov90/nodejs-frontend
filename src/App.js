009344dbd7ee889d8bb3ee5216bcb75b163cf38d
unknown <nazim_huseynov@example.com>
2023-04-14 11:49:20 +0400

    mofified src/App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey! My name is Nazim and you are welcome to my project :)
+          Hey! My name is Nazim and you are welcome to my projects :)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://log.center"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Log Center
         </a>
       </header>
     </div>
